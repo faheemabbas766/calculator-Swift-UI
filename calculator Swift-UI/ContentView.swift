ce631142d5510e1f9dbced4cdd0e4949fe432d35
Faheem Abbas <faheemabbas766@gmail.com>
2025-10-03 14:16:53 +0500

    Initial Commit

---
+++ calculator Swift-UI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  calculator Swift-UI
+//
+//  Created by Faheem Abbas on 10/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ calculator Swift-UI/calculator_Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculator_Swift_UIApp.swift
+//  calculator Swift-UI
+//
+//  Created by Faheem Abbas on 10/3/25.
+//
+
+import SwiftUI
+
+@main
+struct calculator_Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
